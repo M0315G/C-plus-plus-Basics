b6d4c204ccab32a60289a9c629f820750a448b43
Manish Gupta <manish15gupta03@gmail.com>
2021-01-28 20:00:11 +0530

    Basic Notes on C++ STL

---
+++ STL/stl-1.cpp
@@ -0,0 +1,8 @@
+// Topic: STL Introduction in C++
+
+// Notes:
+// 1. STL: Standard Template Library
+// 2. It consist of three components
+//      a. Container: To store your data.
+//      b. Iterator: Helps us to move around the container.
+//      c. Algorithm: Consist of various standard algorithms to ease the process
